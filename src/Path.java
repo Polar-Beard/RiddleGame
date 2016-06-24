63fd86bc75e24ca6c097ba5fe7a25b4085ec78ee
Polar-Beard <saramontecino@outlook.com>
2016-06-24 00:33:37 -0700

    Add path class

---
+++ src/Path.java
@@ -0,0 +1,11 @@
+public class Path
+{
+  private Checkpoint origin;
+  private Checkpoint destination;
+  private String step;
+
+  public Path(Checkpoint origin)
+  {
+    this.origin = origin;
+  }
+}
